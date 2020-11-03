5cdc601b05887cc719be49deb3ff30f95384949c
eiherrera <eiherrera@uc.cl>
2020-11-02 22:46:55 -0300

    Add multiple images data as object properties

--- projector.py
+++ projector.py
@@ -185,7 +185,6 @@ class Projector:
         self._info('Initializing optimization state...')
         dlatents = np.tile(self._dlatent_avg, [self._minibatch_size, 1, 1])
         basic_keys = {self._dlatents_var: dlatents}
-        for _target_image_key, target_image in zip(self.target_images_keys, processed_target_images):
         dinamic_keys = {getattr(self, _target_image_key): target_image for (_target_image_key, target_image) in zip(self.target_images_keys, processed_target_images)}
         tflib.set_vars({**basic_keys, **dinamic_keys})
         # for (_target_image_key, target_image) in zip(self.target_images_keys, target_images)
